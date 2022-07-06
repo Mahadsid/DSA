6b1d0d75f5aac8695078dd3624c155240fc0c1a8
Muhammad Mahad <65539715+Mahadsid@users.noreply.github.com>
2022-07-06 11:11:15 +0530

    Add files via upload

---
+++ Lecture039 Recursion Day9/permutiationofastring.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<map>
+#include<vector>
+#include<stack>
+using namespace std;
+class Solution {
+private:
+    void solve(vector<int> nums, vector<vector<int>> &ans, int index ) {
+        
+        //base case
+        if(index >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        
+        
+        
+        for(int j=index; j<nums.size(); j++) {
+            swap(nums[index], nums[j]);
+            solve(nums, ans, index+1);
+            //backtrack
+            swap(nums[index], nums[j]);
+        }
+        
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if(nums.size()==0)
+            return ans;
+        string output;
+        int index = 0;
+        
+        solve(nums, ans, index);
+        return ans;
+    }
+};
